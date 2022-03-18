215da06059e7f59045d1fd395e4082d7bf28e570
eren <erndemir.1@gmail.com>
2022-03-18 18:06:40 +0300

    commit

--- bin/dart_socket_api.dart
+++ bin/dart_socket_api.dart
@@ -1,3 +1,27 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'package:socket_io/socket_io.dart';
+
+main() {
+  // Dart server
+  var io = Server();
+  io.on('connection', (client) {
+    print('connection');
+    client.on('isConnected', (data) {
+      print('Result => $data');
+      client.emit('connectionStatus', "success");
+    });
+
+    client.on('createOrderSend', (data) {
+      print('Result => $data');
+      client.emit('createOrderResponse', data);
+    });
+    client.on('updateOrderSend', (data) {
+      print('Result => $data');
+      client.emit('updateOrderResponse', data);
+    });
+    client.on('deleteOrderSend', (data) {
+      print('Result => $data');
+      client.emit('deleteOrderResponse', data);
+    });
+  });
+  io.listen(8083);
 }
